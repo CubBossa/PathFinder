1eeb3ffc261d54835e2b945adfdbaf4588c19d2c
CubBossa <leonard.bausenwein@web.de>
2021-06-14 22:19:58 +0200

    Korrektur in createAStarRelations

--- src/main/java/de/bossascrew/pathfinder/util/AStarUtils.java
+++ src/main/java/de/bossascrew/pathfinder/util/AStarUtils.java
@@ -5,6 +5,7 @@ import de.bossascrew.pathfinder.data.RoadMap;
 import de.bossascrew.pathfinder.data.findable.Findable;
 import org.bukkit.Location;
 
+import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,27 +13,30 @@ import java.util.Map;
 public class AStarUtils {
 
     /**
+     * Threadsafe, kann asynchron ausgeführt werden.
      * @param roadMap Die Straßenkarte, aus der der AStar erzeugt wird.
      * @param start   Die Startposition für den Algorithmus. Aus ihr wird die Startnode erzeugt.
      * @param player  Der PathPlayer, für den die Straßenkarte angepasst wird. Für ihn laufen die Permissionabfragen und "Gefunden"-Abfragen
-     * @return Die aus der Spielerinformation erzeugte StartNode des AStar Algorithmus. Sie wird benötigt, um den Algorithmus zu starten.
+     * @return Die aus der Spielerinformation erzeugte StartNode des AStar Algorithmus. Sie wird benötigt, um den Algorithmus zu starten. Der Return-Wert nimmt null an, wenn keine passenden Nodes gefunden wurden.
      */
-    public static AStarNode createAStarRelations(RoadMap roadMap, Location start, PathPlayer player) {
+    public @Nullable
+    static AStarNode createAStarRelations(RoadMap roadMap, Location start, PathPlayer player) {
 
         Collection<Findable> findables = roadMap.getFindables(player);
         Map<Integer, AStarNode> aStarNodes = new HashMap<>();
 
         AStarNode playerNode = new AStarNode(-1, 0);
 
-        double nearestDist = 100;
+        Double nearestDist = null;
         AStarNode nearest = null;
 
         for (Findable findable : findables) {
             double dist = start.distance(findable.getLocation());
             AStarNode aStarNode = new AStarNode(findable.getDatabaseId(), dist);
             aStarNodes.put(findable.getDatabaseId(), aStarNode);
-            if (dist < nearestDist) {
+            if (nearestDist == null || dist < nearestDist) {
                 nearest = aStarNode;
+                nearestDist = dist;
             }
         }
 
