b379e2db32ab42114c60495a656c98f932c9df97
CubBossa <leonard.bausenwein@web.de>
2021-07-12 14:44:55 +0200

    Shop Icon Objekt

--- src/main/java/de/bossascrew/pathfinder/data/Shop.java
+++ src/main/java/de/bossascrew/pathfinder/data/Shop.java
@@ -3,6 +3,7 @@ package de.bossascrew.pathfinder.data;
 import com.comphenix.protocol.injector.BukkitUnwrapper;
 import de.bossascrew.core.util.PluginUtils;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
@@ -14,18 +15,26 @@ import java.util.Map;
 @Getter
 public class Shop {
 
+    @RequiredArgsConstructor
+    @Getter
+    public static class ShopItem {
+        private final ItemStack itemStack;
+        private final String permission;
+        private final double price;
+    }
+
     private final int npcId;
     private final String identifier;
     private final String name;
-    private final Map<ItemStack, String> buyItemStackPermissionMap;
-    private final Map<ItemStack, String> sellItemStackPermissionMap;
+    private final Map<ItemStack, ShopItem> buyItemMap;
+    private final Map<ItemStack, ShopItem> sellItemMap;
 
     public Shop(int npcId, File file) {
         this.npcId = npcId;
         this.identifier = "";
         this.name = "";
-        this.buyItemStackPermissionMap = new HashMap<>();
-        this.sellItemStackPermissionMap = new HashMap<>();
+        this.buyItemMap = new HashMap<>();
+        this.sellItemMap = new HashMap<>();
         PluginUtils.getInstance().runAsync(() -> loadItemStacksAndPermission(file));
     }
 
@@ -39,14 +48,16 @@ public class Shop {
                 ConfigurationSection item = buyItems.getConfigurationSection(itemKey);
                 ItemStack stack = item.getItemStack("item");
                 String permission = item.getString("permission");
-                buyItemStackPermissionMap.put(stack, permission);
+                double price = item.getDouble("trade-price");
+                buyItemMap.put(stack, new ShopItem(stack, permission, price));
             }
             ConfigurationSection sellItems = page.getConfigurationSection("sell-items");
             for(String itemKey : sellItems.getKeys(false)) {
                 ConfigurationSection item = sellItems.getConfigurationSection(itemKey);
                 ItemStack stack = item.getItemStack("item");
                 String permission = item.getString("permission");
-                buyItemStackPermissionMap.put(stack, permission);
+                double price = item.getDouble("trade-price");
+                sellItemMap.put(stack, new ShopItem(stack, permission, price));
             }
         }
     }
--- src/main/java/de/bossascrew/pathfinder/data/findable/TraderFindable.java
+++ src/main/java/de/bossascrew/pathfinder/data/findable/TraderFindable.java
@@ -1,12 +1,18 @@
 package de.bossascrew.pathfinder.data.findable;
 
 import de.bossascrew.pathfinder.data.RoadMap;
+import de.bossascrew.pathfinder.data.Shop;
 import de.bossascrew.pathfinder.util.hooks.CitizensUtils;
-import net.citizensnpcs.api.npc.NPC;
+import lombok.Getter;
+import lombok.Setter;
 
 public class TraderFindable extends NpcFindable {
 
     public static final String SCOPE = "TRADER";
+
+    @Getter
+    @Setter
+    private Shop shop;
 
     public TraderFindable(int databaseId, RoadMap roadMap, int npcId) {
         super(databaseId, roadMap, CitizensUtils.getNPC(npcId));
