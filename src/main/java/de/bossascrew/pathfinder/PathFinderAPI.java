a7b9f25326fe79c8b2ba9383eda31d49603a3f39
CubBossa <leonard.bausenwein@web.de>
2021-07-22 14:07:30 +0200

    API

--- src/main/java/de/bossascrew/pathfinder/PathFinderAPI.java
+++ src/main/java/de/bossascrew/pathfinder/PathFinderAPI.java
@@ -1,66 +1,178 @@
 package de.bossascrew.pathfinder;
 
+import de.bossascrew.pathfinder.data.PathPlayer;
 import de.bossascrew.pathfinder.data.RoadMap;
 import de.bossascrew.pathfinder.data.findable.Findable;
-import de.bossascrew.pathfinder.data.findable.Node;
+import de.bossascrew.pathfinder.data.findable.PlayerFindable;
 import de.bossascrew.pathfinder.data.visualisation.EditModeVisualizer;
 import de.bossascrew.pathfinder.data.visualisation.PathVisualizer;
+import de.bossascrew.pathfinder.handler.PathPlayerHandler;
+import de.bossascrew.pathfinder.handler.RoadMapHandler;
+import de.bossascrew.pathfinder.handler.VisualizerHandler;
+import de.bossascrew.pathfinder.util.AStarUtils;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-public interface PathFinderAPI {
+import javax.annotation.Nullable;
+import java.util.Date;
+import java.util.Objects;
 
-    void findFindable(Player player, Findable findable);
+public class PathFinderAPI {
 
-    void forgetFindable(Player player, Findable findable);
+    public void findFindable(Player player, Findable findable) {
+        PathPlayer pathPlayer = PathPlayerHandler.getInstance().getPlayer(player);
+        if (pathPlayer == null) {
+            return;
+        }
+        pathPlayer.find(findable, false, new Date());
+    }
 
-    void findFindableGrouped(Player player, Findable findable);
+    public void forgetFindable(Player player, Findable findable) {
+        PathPlayer pathPlayer = PathPlayerHandler.getInstance().getPlayer(player);
+        if (pathPlayer == null) {
+            return;
+        }
+        pathPlayer.unfind(findable, false);
+    }
 
-    void forgetFindableGrouped(Player player, Findable findable);
+    public void findFindableGrouped(Player player, Findable findable) {
+        PathPlayer pathPlayer = PathPlayerHandler.getInstance().getPlayer(player);
+        if (pathPlayer == null) {
+            return;
+        }
+        pathPlayer.find(findable, true, new Date());
+    }
 
-    void showPath(Findable findable);
+    public void forgetFindableGrouped(Player player, Findable findable) {
+        PathPlayer pathPlayer = PathPlayerHandler.getInstance().getPlayer(player);
+        if (pathPlayer == null) {
+            return;
+        }
+        pathPlayer.unfind(findable, true);
+    }
 
-    void cancelAllPaths(Player player);
+    public void showPath(Player player, Findable findable) {
+        PathPlayer pPlayer = PathPlayerHandler.getInstance().getPlayer(player.getUniqueId());
+        AStarUtils.startPath(pPlayer, new PlayerFindable(player, findable.getRoadMap()), findable, true);
+    }
 
-    void startEditMode(Player player, RoadMap roadMap);
+    public void cancelAllPaths(Player player) {
+        PathPlayer pathPlayer = PathPlayerHandler.getInstance().getPlayer(player.getUniqueId());
+        if (pathPlayer == null) {
+            return;
+        }
+        pathPlayer.cancelPaths();
+    }
 
-    void stopEditMode(Player player, RoadMap roadMap);
+    public void startEditMode(Player player, RoadMap roadMap) {
+        roadMap.setEditMode(player.getUniqueId(), true);
+    }
 
-    void stopAllEditModes(Player player);
+    public void stopEditMode(Player player, RoadMap roadMap) {
+        roadMap.setEditMode(player.getUniqueId(), false);
+    }
 
-    void selectRoadMap(CommandSender sender, RoadMap roadMap);
+    public void stopAllEditModes(RoadMap roadMap) {
+        roadMap.cancelEditModes();
+    }
 
-    void unselectRoadMap(CommandSender sender);
+    public void stopAllEditModes() {
+        for (RoadMap rm : RoadMapHandler.getInstance().getRoadMaps()) {
+            rm.cancelEditModes();
+        }
+    }
 
-    void unselectRoadMapIfSelected(CommandSender sender, RoadMap roadMap);
+    public void selectRoadMap(CommandSender sender, RoadMap roadMap) {
+        PathPlayer pathPlayer = PathPlayerHandler.getInstance().getPlayer(sender);
+        if (pathPlayer == null) {
+            return;
+        }
+        pathPlayer.setSelectedRoadMap(roadMap.getDatabaseId());
+    }
 
-    Findable getFindable(RoadMap roadMap, String name);
+    public void unselectRoadMap(CommandSender sender) {
+        PathPlayer pathPlayer = PathPlayerHandler.getInstance().getPlayer(sender);
+        if (pathPlayer == null) {
+            return;
+        }
+        pathPlayer.deselectRoadMap();
+    }
 
-    Findable getFindable(int databaseId);
+    public void unselectRoadMapIfSelected(CommandSender sender, RoadMap roadMap) {
+        PathPlayer pathPlayer = PathPlayerHandler.getInstance().getPlayer(sender);
+        if (pathPlayer == null) {
+            return;
+        }
+        pathPlayer.deselectRoadMap(roadMap.getDatabaseId());
+    }
 
-    Node createNode(String name, RoadMap roadMap, Vector position);
+    public @Nullable
+    Findable getFindable(RoadMap roadMap, String name) {
+        return roadMap.getFindable(name);
+    }
 
-    void deleteFindable(int databaseId);
+    public @Nullable
+    Findable getFindable(int databaseId) {
+        for (RoadMap roadMap : RoadMapHandler.getInstance().getRoadMaps()) {
+            Findable f = roadMap.getFindable(databaseId);
+            if (f != null) {
+                return f;
+            }
+        }
+        return null;
+    }
 
-    void deleteFindable(Findable findable);
+    public Findable createNode(String name, RoadMap roadMap, Vector position) {
+        return roadMap.createNode(position, name);
+    }
 
-    RoadMap getRoadMap(String name);
+    public void deleteFindable(int databaseId) {
+        RoadMapHandler.getInstance().getRoadMaps().forEach(rm -> rm.deleteFindable(databaseId));
+    }
 
-    RoadMap getRoadMap(int databaseId);
+    public void deleteFindable(Findable findable) {
+        findable.getRoadMap().deleteFindable(findable.getDatabaseId());
+    }
 
-    RoadMap createRoadMap(String name, World world);
+    public @Nullable
+    RoadMap getRoadMap(String name) {
+        return RoadMapHandler.getInstance().getRoadMap(name);
+    }
 
-    void deleteRoadMap(int databaseId);
+    public @Nullable
+    RoadMap getRoadMap(int databaseId) {
+        return RoadMapHandler.getInstance().getRoadMap(databaseId);
+    }
 
-    void deleteRoadMap(RoadMap roadMap);
+    public RoadMap createRoadMap(String name, World world) {
+        return RoadMapHandler.getInstance().createRoadMap(name, world, false);
+    }
 
-    EditModeVisualizer createEditModeVisualizer(String name, int parentId);
+    public void deleteRoadMap(int databaseId) {
+        RoadMapHandler.getInstance().deleteRoadMap(Objects.requireNonNull(RoadMapHandler.getInstance().getRoadMap(databaseId)));
+    }
 
-    void deleteEditModeVisualizer(int databaseId);
+    public void deleteRoadMap(RoadMap roadMap) {
+        RoadMapHandler.getInstance().deleteRoadMap(roadMap);
+    }
 
-    PathVisualizer createPathVisualizer(String name, int parentId);
+    public EditModeVisualizer createEditModeVisualizer(String name, int parentId) {
+        EditModeVisualizer parent = VisualizerHandler.getInstance().getEditModeVisualizer(parentId);
+        return VisualizerHandler.getInstance().createEditModeVisualizer(name, parent, null, null, null, null, null, null);
+    }
 
-    void deletePathVisualizer(int databaseId);
+    void deleteEditModeVisualizer(int databaseId) {
+        VisualizerHandler.getInstance().deleteEditModeVisualizer(databaseId);
+    }
+
+    PathVisualizer createPathVisualizer(String name, int parentId) {
+        PathVisualizer parent = VisualizerHandler.getInstance().getPathVisualizer(parentId);
+        return VisualizerHandler.getInstance().createPathVisualizer(name, parent, null, null, null, null, null);
+    }
+
+    void deletePathVisualizer(int databaseId) {
+        VisualizerHandler.getInstance().deletePathVisualizer(databaseId);
+    }
 }
