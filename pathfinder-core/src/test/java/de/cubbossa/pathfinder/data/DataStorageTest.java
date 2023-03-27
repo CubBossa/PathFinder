47dfff9e793137d2e1710710229cecc4db29fdb9
CubBossa <leonard.bausenwein@web.de>
2023-03-27 19:05:33 +0200

    #33 progress: more database tests

--- pathfinder-core/src/test/java/de/cubbossa/pathfinder/data/DataStorageTest.java
+++ pathfinder-core/src/test/java/de/cubbossa/pathfinder/data/DataStorageTest.java
@@ -10,6 +10,9 @@ import de.cubbossa.pathfinder.core.node.implementation.Waypoint;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -26,6 +29,7 @@ public abstract class DataStorageTest {
   abstract DataStorage storage();
 
   protected static MiniMessage miniMessage;
+  protected static World world;
   protected DataStorage storage;
   protected NodeTypeRegistry nodeTypeRegistry;
   protected NodeType<Waypoint> waypointNodeType;
@@ -34,6 +38,7 @@ public abstract class DataStorageTest {
   static void beforeAll() {
     miniMessage = MiniMessage.miniMessage();
     MockBukkit.mock();
+    world = new WorldMock(Material.DIRT, 1);
   }
 
   @AfterEach
@@ -50,35 +55,63 @@ public abstract class DataStorageTest {
     storage.connect();
   }
 
-  @Test
-  void createNode() {
-
-    World world = new WorldMock(Material.DIRT, 1);
+  private Waypoint waypoint() {
     CompletableFuture<Waypoint> future = storage.createNode(waypointNodeType, new Location(world, 1, 2, 3));
     Waypoint waypoint = future.join();
 
     Assertions.assertFalse(future.isCompletedExceptionally());
     Assertions.assertNotNull(waypoint);
 
-    Collection<Node<?>> nodesAfter = storage.getNodes().join();
-    Assertions.assertEquals(1, nodesAfter.size());
+    return waypoint;
   }
 
   @Test
-  void getNodes() {
-    CompletableFuture<Collection<Node<?>>> future = storage.getNodes();
-    Collection<Node<?>> nodes = future.join();
+  void createNode() throws ExecutionException, InterruptedException, TimeoutException {
+
+    CompletableFuture<Waypoint> future = storage.createNode(waypointNodeType, new Location(world, 1, 2, 3));
+    Waypoint waypoint = future.get(1, TimeUnit.SECONDS);
 
     Assertions.assertFalse(future.isCompletedExceptionally());
-    Assertions.assertEquals(0, nodes.size());
-
-    World world = new WorldMock(Material.DIRT, 1);
-    Waypoint waypoint = storage.createNode(waypointNodeType, new Location(world, 1, 2, 3)).join();
     Assertions.assertNotNull(waypoint);
 
     Collection<Node<?>> nodesAfter = storage.getNodes().join();
     Assertions.assertEquals(1, nodesAfter.size());
   }
 
+  @Test
+  void getNodes() throws ExecutionException, InterruptedException, TimeoutException {
+    CompletableFuture<Collection<Node<?>>> future = storage.getNodes();
+    Collection<Node<?>> nodes = future.join();
 
+    Assertions.assertFalse(future.isCompletedExceptionally());
+    Assertions.assertEquals(0, nodes.size());
+
+    waypoint();
+
+    Collection<Node<?>> nodesAfter = storage.getNodes().get(1, TimeUnit.SECONDS);
+    Assertions.assertEquals(1, nodesAfter.size());
+  }
+
+  @Test
+  void getNodeType() throws ExecutionException, InterruptedException, TimeoutException {
+
+    CompletableFuture<NodeType<?>> future1 = storage.getNodeType(waypoint().getNodeId());
+    NodeType<?> type = future1.get(1, TimeUnit.SECONDS);
+
+    Assertions.assertFalse(future1.isCompletedExceptionally());
+    Assertions.assertNotNull(type);
+    Assertions.assertEquals(waypointNodeType, type);
+  }
+
+  @Test
+  void updateNode() throws ExecutionException, InterruptedException, TimeoutException {
+    Waypoint waypoint = waypoint();
+    storage.updateNode(waypoint.getNodeId(), node -> {
+      node.setLocation(waypoint.getLocation().clone().add(0, 2, 0));
+    }).get(1, TimeUnit.SECONDS);
+
+    Node<?> after = storage.getNode(waypoint.getNodeId()).get(1, TimeUnit.SECONDS);
+
+    Assertions.assertEquals(waypoint.getLocation().add(0, 2, 0), after.getLocation());
+  }
 }
