64c554f6cf9f61688c26f2dc94245a420e0e6258
CubBossa <leonard.bausenwein@web.de>
2023-07-23 09:47:30 +0200

    only use 1.19.4 editmode if version enabled

--- pathfinder-editmode/src/main/java/de/cubbossa/pathfinder/editmode/DefaultNodeGroupEditorFactory.java
+++ pathfinder-editmode/src/main/java/de/cubbossa/pathfinder/editmode/DefaultNodeGroupEditorFactory.java
@@ -11,6 +11,7 @@ import de.cubbossa.pathfinder.editmode.renderer.EdgeArmorStandRenderer;
 import de.cubbossa.pathfinder.editmode.renderer.NodeArmorStandRenderer;
 import de.cubbossa.pathfinder.editmode.renderer.NodeGroupListRenderer;
 import de.cubbossa.pathfinder.editmode.renderer.ParticleEdgeRenderer;
+import de.cubbossa.pathfinder.util.Version;
 import org.bukkit.entity.Player;
 
 @AutoService(NodeGroupEditorFactory.class)
@@ -21,9 +22,13 @@ public class DefaultNodeGroupEditorFactory implements NodeGroupEditorFactory {
     DefaultNodeGroupEditor editor = new DefaultNodeGroupEditor(group);
     editor.getRenderers().add(new ParticleEdgeRenderer(pathFinder.getConfiguration().getEditMode()));
     editor.getRenderers().add(new NodeArmorStandRenderer(PathFinderPlugin.getInstance()));
-    editor.getRenderers().add(new NodeGroupListRenderer(PathFinderPlugin.getInstance(), 15, 8));
-    // editor.getRenderers().add(new NodeDisplayRenderer());
     editor.getRenderers().add(new EdgeArmorStandRenderer(PathFinderPlugin.getInstance()));
+
+    if (new Version(PathFinderPlugin.getInstance().getServer().getVersion()).compareTo(new Version("1.19.4")) >= 0) {
+      editor.getRenderers().add(new NodeGroupListRenderer(PathFinderPlugin.getInstance(), 15, 8));
+      // editor.getRenderers().add(new NodeDisplayRenderer());
+    }
+
     return editor;
   }
 }
