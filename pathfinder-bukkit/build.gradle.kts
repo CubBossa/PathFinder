3bf5b136f7cdd4af6e67e9c761abfe0ddb2623c3
CubBossa <leonard.bausenwein@web.de>
2023-08-02 09:21:48 +0200

    version increment

--- pathfinder-bukkit/build.gradle.kts
+++ pathfinder-bukkit/build.gradle.kts
@@ -107,8 +107,8 @@ bukkit {
         register("pathfinder.command.find") { default = BukkitPluginDescription.Permission.Default.NOT_OP }
         register("pathfinder.command.findlocation")
         register("pathfinder.command.findplayer.request")
-        register("pathfinder.command.findplayer.accept") { default = BukkitPluginDescription.Permission.Default.NOT_OP }
-        register("pathfinder.command.findplayer.decline") { default = BukkitPluginDescription.Permission.Default.NOT_OP }
+        register("pathfinder.command.findplayer.accept")
+        register("pathfinder.command.findplayer.decline")
         register("pathfinder.command.cancel_path") { default = BukkitPluginDescription.Permission.Default.NOT_OP }
         register("pathfinder.command.roadmap.info")
         register("pathfinder.command.roadmap.create")
