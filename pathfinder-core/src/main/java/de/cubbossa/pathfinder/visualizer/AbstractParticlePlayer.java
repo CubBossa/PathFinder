df6118dd5ec6327f585701063c3cc4f08e5e1337
bale1017 <bale1017@h-ka.de>
2023-09-28 10:57:02 +0200

    progress on particle player (#30, #69)

--- pathfinder-core/src/main/java/de/cubbossa/pathfinder/visualizer/AbstractParticlePlayer.java
+++ pathfinder-core/src/main/java/de/cubbossa/pathfinder/visualizer/AbstractParticlePlayer.java
@@ -1,31 +1,27 @@
 package de.cubbossa.pathfinder.visualizer;
 
 import de.cubbossa.pathapi.misc.Location;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.Setter;
 
+/**
+ * The particle player instance handles refreshing paths in a smooth way. At the same time, it reduces bandwidth by
+ * only sending packets that are in range of the player.
+ * The particle player remembers old paths and continues the illusion of wandering particles by playing initiated steps
+ * on the older paths. It does not add "wandering particles" to the old path and only continues already existing ones.
+ * New "wandering particles" will always be rendered on the newest path.
+ * If a "wandering particle" leaves the view distance of the player, it gets removed and won't be visible, even if the
+ * player becomes in range again later.
+ *
+ * @param <LocationT> An abstraction of the actual location class implementation. For Bukkit org.bukkit.Location.
+ */
 public abstract class AbstractParticlePlayer<LocationT> extends TimerTask {
-
-  private record Point<LocationT>(Location internalLocation, LocationT platformLocation, int index) {
-  }
-
-  interface PathSupplier<LocationT> {
-
-    /**
-     * @param view     The view that is receiving particles.
-     * @param distance The maximal allowed distance to the view
-     * @return A location-index map, because the step attribute is working with particle indices and when removing particles,
-     * the index must somehow be preserved.
-     */
-    List<Point<LocationT>> getPath(Location view, double distance);
-
-  }
 
   /**
    * Interval in milliseconds
@@ -33,47 +29,73 @@ public abstract class AbstractParticlePlayer<LocationT> extends TimerTask {
   @Getter
   @Setter
   private int interval;
+
   /**
    * Amount of points to leave out before rendering the next point
    */
   @Getter
   @Setter
   private int steps;
+
   /**
    * rendering distance for player
    */
   @Getter
   @Setter
-  private int distance;
+  private int distance = 16 * 8;
+  /**
+   * The amount of boundary steps to increment for each step. If set to 1 (default), one "wandering particle"
+   * will be continued to the end, while a new one wanders on the updated path.
+   */
+  @Getter
+  @Setter
+  private int updateIncrement = 1;
 
-  private final PathSupplier<LocationT> pathSupplier;
   private Timer timer;
   private final AtomicInteger currentStep = new AtomicInteger(0);
-  private final List<Point<LocationT>> path;
 
-  private Location lastView = null;
-  private List<Point<LocationT>> lastPath = new ArrayList<>();
+  private AbstractParticleTrailPlayer<LocationT> newestPath;
+  private List<AbstractParticleTrailPlayer<LocationT>> oldPaths = new ArrayList<>();
+
+  private List<Location> pathUpdate = null;
 
   public AbstractParticlePlayer(List<Location> path) {
-    this.path = new ArrayList<>();
-    int i = 0;
-    for (Location loc : path) {
-      this.path.add(new Point<>(loc, convert(loc), i++));
-    }
-
-    this.pathSupplier = (view, d) -> {
-      double squaredDistance = Math.pow(d, 2);
-      return this.path.stream().parallel()
-          .filter(e -> e.internalLocation().distanceSquared(view) < squaredDistance)
-          .toList();
-    };
+    this.newestPath = new AbstractParticleTrailPlayer<>(this, path);
   }
 
+  /**
+   * Checks, if one period elapsed since the last time that the path had been changed.
+   * Only if this is the case, the current path gets refreshed. Refreshed means, that all
+   * "wandering particles" produced by the steps continue their original path, but new
+   * "wandering particles" follow the newly set path.
+   *
+   * @param path The path list.
+   */
+  public void setNewestPath(List<Location> path) {
+    this.pathUpdate = path;
+  }
+
+  public void setNewestPathAndConvert(List<LocationT> path) {
+    setNewestPath(path.stream()
+        .map(this::convert)
+        .collect(Collectors.toList()));
+  }
+
+  /**
+   * @return The location of the viewer that limits particles by view distance.
+   */
   abstract Location getView();
 
+  /**
+   * Plays a particle at a location
+   *
+   * @param location the location to play the particle at.
+   */
   abstract void playParticle(LocationT location);
 
   abstract LocationT convert(Location location);
+
+  abstract Location convert(LocationT location);
 
   public void start() {
     if (timer == null) {
@@ -93,19 +115,29 @@ public abstract class AbstractParticlePlayer<LocationT> extends TimerTask {
 
   @Override
   public void run() {
-    Location view = getView();
     int s = currentStep.getAndIncrement();
+    // Make sure to apply new path
+    if (s == 0 && pathUpdate != null) {
+      // Add the previously newest path to the old paths
+      // Make path update the newest path
+      oldPaths.add(newestPath);
+      newestPath.resetBounds();
+      newestPath = new AbstractParticleTrailPlayer<>(this, pathUpdate);
+      newestPath.setLowerBound(0);
+      newestPath.setUpperBound(updateIncrement);
+      pathUpdate = null;
+    }
     if (s + 1 >= steps) {
       currentStep.set(0);
     }
-    List<Point<LocationT>> path = lastView.toBlockCoordinates().equals(view.toBlockCoordinates())
-        ? lastPath
-        : pathSupplier.getPath(view, distance);
-    lastView = view;
-    lastPath = path;
 
-    path.stream().parallel()
-        .filter(e -> e.index() % steps == s)
-        .forEach(e -> playParticle(e.platformLocation()));
+    newestPath.setUpperBound(newestPath.getUpperBound() + updateIncrement);
+    newestPath.run(s, steps);
+
+    oldPaths = oldPaths.stream()
+        .peek(p -> p.setLowerBound(p.getLowerBound() + updateIncrement))
+        .filter(p -> p.getUpperBound() > p.getLowerBound())
+        .peek(p -> p.run(s, steps))
+        .collect(Collectors.toCollection(ArrayList::new));
   }
 }
---
+++ pathfinder-core/src/main/java/de/cubbossa/pathfinder/visualizer/AbstractParticleTrailPlayer.java
@@ -0,0 +1,69 @@
+package de.cubbossa.pathfinder.visualizer;
+
+import de.cubbossa.pathapi.misc.Location;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+public class AbstractParticleTrailPlayer<LocationT> {
+
+  private final AbstractParticlePlayer<LocationT> owner;
+
+  @Getter
+  @Setter
+  private int lowerBound = 0;
+  @Getter
+  @Setter
+  private int upperBound = 0;
+  private final Point<LocationT>[] points;
+
+  public AbstractParticleTrailPlayer(AbstractParticlePlayer<LocationT> owner, List<Location> points) {
+    this.owner = owner;
+    this.points = new Point[points.size()];
+    int i = 0;
+    for (Location p : points) {
+      this.points[i] = new Point<>(p, owner.convert(p), i++);
+    }
+  }
+
+  public AbstractParticleTrailPlayer(AbstractParticleTrailPlayer<LocationT> other) {
+    this.owner = other.owner;
+    this.points = Arrays.copyOf(other.points, other.points.length);
+    this.lowerBound = other.lowerBound;
+    this.upperBound = other.upperBound;
+  }
+
+  public void run() {
+    run(1, 1);
+  }
+
+  public void run(int everyNth, int outOf) {
+    Location view = owner.getView();
+
+    double distSqr = Math.pow(owner.getDistance(), 2);
+
+    asBoundedList().stream().parallel()
+        .filter(e -> e.index() % outOf == everyNth)
+        .filter(e -> e.internalLocation().distanceSquared(view) < distSqr)
+        .forEach(e -> owner.playParticle(e.platformLocation()));
+  }
+
+  public List<Point<LocationT>> asList() {
+    return new LinkedList<>(Arrays.asList(points));
+  }
+
+  public List<Point<LocationT>> asBoundedList() {
+    return new LinkedList<>(Arrays.asList(points).subList(lowerBound, upperBound));
+  }
+
+  public void resetBounds() {
+    this.lowerBound = 0;
+    this.upperBound = points.length;
+  }
+
+  record Point<LocationT>(Location internalLocation, LocationT platformLocation, int index) {
+  }
+}
