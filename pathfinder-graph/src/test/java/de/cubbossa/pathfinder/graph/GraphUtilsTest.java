e377c869e69208e77f17e4daed934396a37957f6
CubBossa <leonard.bausenwein@web.de>
2024-07-03 13:00:42 +0200

    #82 test for island function

---
+++ pathfinder-graph/src/test/java/de/cubbossa/pathfinder/graph/GraphUtilsTest.java
@@ -0,0 +1,31 @@
+package de.cubbossa.pathfinder.graph;
+
+import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("UnstableApiUsage")
+class GraphUtilsTest {
+
+  @Test
+  void islands() {
+
+    MutableValueGraph<String, Double> aToB = ValueGraphBuilder.directed().build();
+    aToB.addNode("a");
+    aToB.addNode("b");
+    aToB.putEdgeValue("a", "b", 1.5);
+    Assertions.assertEquals(1, GraphUtils.islands(aToB).size());
+
+    MutableValueGraph<String, Double> bToA = ValueGraphBuilder.directed().build();
+    bToA.addNode("a");
+    bToA.addNode("b");
+    bToA.putEdgeValue("a", "b", 1.5);
+    Assertions.assertEquals(1, GraphUtils.islands(bToA).size());
+
+    MutableValueGraph<String, Double> aAndB = ValueGraphBuilder.directed().build();
+    aAndB.addNode("a");
+    aAndB.addNode("b");
+    Assertions.assertEquals(2, GraphUtils.islands(aAndB).size());
+  }
+}
